5c140b85a8998dbb770531f785af15b2c96b8def
Justin Marrington <justin@bigfish.tv>
2014-12-02 12:22:18 +1000

    Test stubbing for API

---
+++ test/api-spec.js
@@ -0,0 +1,18 @@
+var periscope = require('../periscope.js');
+
+describe('Periscope parsing API', function () {
+  var periscope;
+
+  describe('Parse method', function() {
+    beforeEach(function () {
+    });
+
+    it('should accept .md or .markdown files for parsing', function () {
+      
+    });
+
+    it('should accept .html or .htm files for parsing', function () {
+      
+    });
+  });
+});
